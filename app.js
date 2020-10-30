68bd2665faec9465dfcbb50cfd1a781529a4f650
geigea84 <geigea84@gmail.com>
2020-10-29 21:01:20 -0500

    added inquirer

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const inquirer = require("inquirer");
+/*
 const fs = require("fs");
 const generatePage = require("./src/page-template.js");
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const pageHTML = generatePage(name, github);
+*/
 
 /*9.1
 const printProfileData = (profileDataArr) => {
@@ -24,8 +26,20 @@ printProfileData(profileDataArgs);
  write onto the file, in this case the HTML template literal. The last parameter is a 
  callback function that will be used for error handling.*/
 
- fs.writeFile("./index.html", generatePage(name, github), err => {
+/*
+ fs.writeFile("./index.html", pageHTML, err => {
      if (err) throw err;
 
      console.log("Portfolio complete!  Check out index.html to see the output!")
  });
+*/
+
+inquirer
+ .prompt([
+     {
+         type: "input",
+         name: "name",
+         message: "What is your name?"
+     }
+ ])
+ .then(answers => console.log(answers));
