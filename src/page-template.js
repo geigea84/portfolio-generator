0707b549b0d2a1a13a3d8271700e6c52c4fc59d7
geigea84 <geigea84@gmail.com>
2020-10-30 21:07:05 -0500

    finished 9.4

--- src/page-template.js
+++ src/page-template.js
@@ -28,60 +28,43 @@ based on whether their feature property was true or false. Once we separated the
 array data, we created two sets of HTML data and got them into the <section> 
 element.*/
 const generateProjects = projectsArr => {
-    //get array of featured projects
-    const featuredProjects = projectsArr.filter(project => {
-        if (project.feature) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    });
-
-    //get array of non-featured projects
-    const nonFeaturedProjects = projectsArr.filter(project => {
-        if (!project.feature) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    });
-
-    const featuredProjectHtmlArr = featuredProjects.map(({name, description, languages, link}) => {
-        return `
-            <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
-                <h3 class="portfolio-item-title text-light">${name}</h3>
-                <h5 class="portfolio-languages">
-                    Built With:
-                    ${languages.join(", ")}
-                </h5>
-                <p>${description}</p>
-                <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-            </div>
-        `;
-    });
-
-    const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(({name, description, languages, link}) => {
-        return `
-            <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
-                <h3 class="portfolio-item-title text-light">${name}</h3>
-                <h5 class="portfolio-languages">
-                    Built With:
-                    ${languages.join(", ")}
-                </h5>
-                <p>${description}</p>
-                <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-            </div>
-        `;
-    });
-
     return `
         <section class="my-3" id="portfolio">
             <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
             <div class="flex-row justify-space-between">
-            ${featuredProjectHtmlArr.join("")}
-            ${nonFeaturedProjectHtmlArr.join("")}
+            ${projectsArr
+            .filter(({feature}) => feature)
+            .map(({name, description, languages, link}) => {
+              return `
+              <div class="col-12 mb-2 bg-dark text-light p-3">
+                <h3 class="portfolio-item-title text-light">${name}</h3>
+                <h5 class="portfolio-languages">
+                    Built With:
+                    ${languages.join(", ")}
+                </h5>
+                <p>${description}</p>
+                <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+              </div>
+              `;
+            })
+            .join("")}
+
+            ${projectsArr
+            .filter(({feature}) => !feature)
+            .map(({fame, description, languages, link}) => {
+              return `
+              <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+                <h3 class="portfolio-item-title text-light">${name}</h3>
+                <h5 class="portfolio-languages">
+                    Built With:
+                    ${languages.join(", ")}
+                </h5>
+                <p>${description}</p>
+                <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+              </div>
+              `;
+            })
+            .join("")}
             </div>
         </section>
     `;
