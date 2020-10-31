788d2cd57c99cbf0f5dee82b0dc199cdda067d2c
geigea84 <geigea84@gmail.com>
2020-10-30 22:31:23 -0500

    staging to switch branches because this decided to crap out

--- src/page-template.js
+++ src/page-template.js
@@ -18,7 +18,6 @@ First, we take the array of project data and we create a new array from it,
 called projectHtmlArr. Then we take that array and interpolate it into the 
 returning project <section> element template literal. We use a .join() method to 
 turn the projectHtmlArr into a combined string of HTML before returning as well.
-
 For the projects, we use .map() to iterate through the projectArr, we destructure 
 each project's object data based on property name, and we return an entire set of 
 HTML code with it!*/
@@ -28,49 +27,66 @@ based on whether their feature property was true or false. Once we separated the
 array data, we created two sets of HTML data and got them into the <section> 
 element.*/
 const generateProjects = projectsArr => {
-    return `
-        <section class="my-3" id="portfolio">
-            <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-            <div class="flex-row justify-space-between">
-            ${projectsArr
-            .filter(({feature}) => feature)
-            .map(({name, description, languages, link}) => {
-              return `
-              <div class="col-12 mb-2 bg-dark text-light p-3">
-                <h3 class="portfolio-item-title text-light">${name}</h3>
-                <h5 class="portfolio-languages">
-                    Built With:
-                    ${languages.join(", ")}
-                </h5>
-                <p>${description}</p>
-                <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-              </div>
-              `;
-            })
-            .join("")}
+  //get array of featured projects
+  const featuredProjects = projectsArr.filter(project => {
+      if (project.feature) {
+          return true;
+      }
+      else {
+          return false;
+      }
+  });
 
-            ${projectsArr
-            .filter(({feature}) => !feature)
-            .map(({name, description, languages, link}) => {
-              return `
-              <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-                <h3 class="portfolio-item-title text-light">${name}</h3>
-                <h5 class="portfolio-languages">
-                    Built With:
-                    ${languages.join(", ")}
-                </h5>
-                <p>${description}</p>
-                <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-              </div>
-              `;
-            })
-            .join("")}
-            </div>
-        </section>
-    `;
+  //get array of non-featured projects
+  const nonFeaturedProjects = projectsArr.filter(project => {
+      if (!project.feature) {
+          return true;
+      }
+      else {
+          return false;
+      }
+  });
+
+  const featuredProjectHtmlArr = featuredProjects.map(({name, description, languages, link}) => {
+      return `
+          <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
+              <h3 class="portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                  Built With:
+                  ${languages.join(", ")}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+      `;
+  });
+
+  const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(({name, description, languages, link}) => {
+      return `
+          <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
+              <h3 class="portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                  Built With:
+                  ${languages.join(", ")}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+      `;
+  });
+
+  return `
+      <section class="my-3" id="portfolio">
+          <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+          <div class="flex-row justify-space-between">
+          ${featuredProjectHtmlArr.join("")}
+          ${nonFeaturedProjectHtmlArr.join("")}
+          </div>
+      </section>
+  `;
 };
 
-/* 9.2.4 This function returns a string. Let's break it down further to compare 
+/* 9.2.4 This function returns a string. Let's break it down further to compare
 it to the arrow functions we created in the previous lesson. You might remember 
 that parentheses are unnecessary in arrow functions when there is one parameter. 
 In this function, which has no parameters, we need parentheses to hold the place 
